6e35751a5d9399bef0167884ec57b3949f61f28d
Zack <zack.scholl@gmail.com>
2024-01-12 09:50:16 -0800

    fix: handle case for midi timing
    the code previously would miss 0xf5, 0xf6, and 0xf7. not sure if this is the change that is best, but just wanted to bring it to your attention!

--- midibox/midi.c
+++ midibox/midi.c
@@ -4,7 +4,7 @@ midi_message midi_read(midi_parser *p, uint8_t b) {
 	enum { DATA0_PRESENT = 0x80 };
 	midi_message msg = {0};
 
-	if (b >= 0xf8) {
+	if (b == 0xf8) {
 		msg.status = b;
 	} else if (b >= 0xf4) {
 		msg.status = b;
